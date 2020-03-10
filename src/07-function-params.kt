f79d39d5c338bacf8a1128b520d7ee4d717aaa0b
Egide Muhire <saladthieves@gmail.com>
2020-03-10 16:29:32 +0300

    Lesson 07 end

--- src/07-function-params.kt
+++ src/07-function-params.kt
@@ -1,3 +1,18 @@
 fun main() {
+    callPhone("Abebe", 251, "0985697789")
 
+    callPhone(
+        code = 254,
+        number = "0783748474",
+        name = "Abebe"
+    )
+
+    callPhone("Abebe", code = 251)
+//    callPhone(name = "Abebe", 251)    // does not work
+    callPhone("James")
+    callPhone("James", 250)
+}
+
+fun callPhone(name: String = "Abebe", code: Int = 251, number: String = "0985000000") {
+    println("Calling $name on +$code-${number.substring(1)}...")
 }
