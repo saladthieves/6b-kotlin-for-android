388b2b8be9fa0ab36baa7aafbd9e4ee829b6a70c
Egide Muhire <saladthieves@gmail.com>
2020-03-15 16:20:18 +0300

    End of lesson 11

--- src/11-advanced-fun.kt
+++ src/11-advanced-fun.kt
@@ -1,5 +1,26 @@
 data class Player(val username: String, var highScore: Int = 0)
 
+fun Player.win() {
+    highScore++
+}
+
+fun Player.lose() {
+    highScore -= if (highScore > 0) 1 else 0
+}
+
+fun Int.isNegative() = this < 0
+
+infix fun Int.መደመር(a: Int) {
+    println(this + a)
+}
+
 fun main() {
     val player = Player("abebek")
+
+    player.lose()
+    player.win()
+    player.win()
+    println(player.highScore)
+
+    val range = 30.downTo(1).step(4)
 }
